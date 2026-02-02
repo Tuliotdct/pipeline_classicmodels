c407bfa95014df4a5e5e214a49e34200ff09aedc
Tulio Tome <tuliotdct@gmail.com>
2026-02-02 22:26:07 +0100

    feat: add db_connections.py

---
+++ src/db_connections.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy import URL
+from .aws_secrets import get_secret
+
+
+def get_connection():
+
+    secret = get_secret()
+
+    conn = URL.create(
+        drivername='postgresql+psycopg2',
+        username=secret["username"],
+        password=secret["password"],
+        host=secret["host"],
+        port=secret["port"],
+        database=secret["dbname"]
+    )
+
+    engine = create_engine(conn)
+
+    return engine
