95dd72e10fa231ec8b8cd3dada5c70c1cd9817da
Tulio Tome <tuliotdct@gmail.com>
2026-02-10 21:00:12 +0100

    feat: add db_tables.py and db_tables.txt for database table management

---
+++ src/config/db_tables.py
@@ -0,0 +1,16 @@
+from sqlalchemy import inspect
+from ..db_connections import get_connection
+import pandas as pd
+
+
+def get_db_tables():
+    
+    conn = get_connection()
+    insp = inspect(conn)
+    tables = insp.get_table_names()
+
+    df = pd.DataFrame(tables, columns=['table_names'])
+    db_tables_txt = df.to_csv('src/config/db_tables.txt', index=False, header=False)
+    
+
+    return db_tables_txt
