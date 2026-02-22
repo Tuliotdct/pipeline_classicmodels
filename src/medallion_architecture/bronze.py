e2c31161c3dd5c085e5949c6f9a1d10b67c9b639
Tulio Tome <tuliotdct@gmail.com>
2026-02-22 14:41:09 +0100

    refactor: remove redundant spark session and JDBC connection functions

--- src/medallion_architecture/bronze.py
+++ src/medallion_architecture/bronze.py
@@ -1,41 +1,4 @@
-from pyspark.sql import SparkSession
-from src.connections.aws_secrets import get_secret
-from dotenv import load_dotenv
-import os
-
-def create_spark_session():
-
-    load_dotenv()
-
-    aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
-    aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-
-    spark = SparkSession.builder \
-    .appName("classicmodels-extract") \
-    .config("spark.jars.packages",
-            "org.postgresql:postgresql:42.7.1,"
-            "org.apache.hadoop:hadoop-aws:3.4.1,"
-            "com.amazonaws:aws-java-sdk-bundle:1.12.767") \
-    .config("spark.hadoop.fs.s3a.aws.credentials.provider",
-            "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
-    .config("spark.hadoop.fs.s3a.access.key", aws_access_key) \
-    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_key) \
-    .getOrCreate()
-
-    return spark
-
-def spark_jdbc_connection():
-
-    secret = get_secret()
-    jdbc_url = f"jdbc:postgresql://{secret['host']}:{secret['port']}/{secret['dbname']}"
-
-    props = {
-        "user": secret['username'],
-        "password": secret['password'],
-        "driver": "org.postgresql.Driver"
-    }
-
-    return jdbc_url, props
+from src.connections.spark_connections import create_spark_session, spark_jdbc_connection
 
 def create_bronze_for_table(table_name, execution_date=None):
 
