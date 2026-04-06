71abe9751a5847089289def70339340cf18569b4
Tulio Tome <tuliotdct@gmail.com>
2026-04-06 17:58:14 +0200

    feat: enhance create_spark_session and create_silver_table functions for improved S3 integration and Iceberg support

--- src/connections/spark_connections.py
+++ src/connections/spark_connections.py
@@ -9,17 +9,35 @@ def create_spark_session():
 
     aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+    aws_region = os.getenv("REGION_NAME")
+    bucket = os.getenv("S3_BUCKET_NAME")
 
     spark = SparkSession.builder \
     .appName("classicmodels-extract") \
     .config("spark.jars.packages",
             "org.postgresql:postgresql:42.7.1,"
             "org.apache.hadoop:hadoop-aws:3.4.1,"
-            "com.amazonaws:aws-java-sdk-bundle:1.12.767") \
+            "com.amazonaws:aws-java-sdk-bundle:1.12.767,"
+            "org.apache.iceberg:iceberg-spark-runtime-4.0_2.13:1.10.1,"
+            "org.apache.iceberg:iceberg-aws-bundle:1.10.1") \
     .config("spark.hadoop.fs.s3a.aws.credentials.provider",
             "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
     .config("spark.hadoop.fs.s3a.access.key", aws_access_key) \
     .config("spark.hadoop.fs.s3a.secret.key", aws_secret_key) \
+    .config("spark.sql.extensions",
+            "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions") \
+    .config("spark.sql.catalog.glue_catalog",
+            "org.apache.iceberg.spark.SparkCatalog") \
+    .config("spark.sql.catalog.glue_catalog.catalog-impl",
+            "org.apache.iceberg.aws.glue.GlueCatalog") \
+    .config("spark.sql.catalog.glue_catalog.io-impl",
+            "org.apache.iceberg.aws.s3.S3FileIO") \
+    .config("spark.driver.extraJavaOptions", f"-Daws.region={aws_region}") \
+    .config("spark.executor.extraJavaOptions", f"-Daws.region={aws_region}") \
+    .config("spark.sql.optimizer.excludedRules",
+            "org.apache.spark.sql.catalyst.optimizer.SimplifyCasts") \
+    .config("spark.sql.catalog.glue_catalog.warehouse",
+            f"s3a://{bucket}") \
     .getOrCreate()
 
     return spark
--- src/medallion_architecture/silver.py
+++ src/medallion_architecture/silver.py
@@ -1,58 +1,52 @@
 import boto3
-from src.connections.spark_connections import create_spark_session, spark_jdbc_connection
+from src.connections.spark_connections import create_spark_session
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 
 bucket = os.getenv("S3_BUCKET_NAME")
 s3 = boto3.client('s3')
 
-def bronze_tables():
 
+def bronze_tables():
     paginator = s3.get_paginator('list_objects_v2')
     page_iterator = paginator.paginate(Bucket=bucket, Prefix='bronze/', Delimiter='/')
 
-    list_bronze_tables = []
+    tables = []
     for page in page_iterator:
         if 'CommonPrefixes' not in page:
             continue
-        
         for obj in page['CommonPrefixes']:
-            filter_bronze_tables = obj['Prefix'].split('/')[1]
-            list_bronze_tables.append(filter_bronze_tables)
-
-    return list_bronze_tables
+            tables.append(obj['Prefix'].split('/')[1])
+    return tables
 
 
-def last_execution_date(table_name=None):
-    
+def last_execution_date(table_name):
     paginator = s3.get_paginator('list_objects_v2')
     page_iterator = paginator.paginate(Bucket=bucket, Prefix=f'bronze/{table_name}/', Delimiter='/')
 
-    list_execution_date = []
+    dates = []
     for page in page_iterator:
         if 'CommonPrefixes' not in page:
             continue
-        
         for obj in page['CommonPrefixes']:
-            filter_execution_date = obj['Prefix'].split('/')[2]
-            list_execution_date.append(filter_execution_date)
-
-    list_execution_date = max(list_execution_date) 
-
-    return list_execution_date
+            dates.append(obj['Prefix'].split('/')[2])
+    return max(dates)
 
 
-def create_current_silver_for_table(table_name):
-    current_execution_date = last_execution_date(table_name)
+def create_silver_table(table_name):
+    execution_date = last_execution_date(table_name)
     spark = create_spark_session()
     try:
-        df = spark.read.parquet(f"s3a://{bucket}/bronze/{table_name}/{current_execution_date}")
-        df.write.mode("overwrite").parquet(f"s3a://{bucket}/silver_current/{table_name}")
-        return {"table": table_name, "execution_date": current_execution_date, "records": df.count(), "status": "success"}
+        df = spark.read.parquet(f"s3a://{bucket}/bronze/{table_name}/{execution_date}")
+        spark.sql(f"CREATE DATABASE IF NOT EXISTS glue_catalog.silver LOCATION 's3a://{bucket}/silver'")
+        df.writeTo(f"glue_catalog.silver.{table_name}") \
+          .tableProperty("write.format.default", "parquet") \
+          .createOrReplace()
+        return {"table": table_name, "execution_date": execution_date, "status": "success"}
     finally:
         spark.stop()
 
 
+print(create_silver_table("customers"))
