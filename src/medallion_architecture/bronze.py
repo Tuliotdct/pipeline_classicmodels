0a3b2b4544b8f4084d3346956d162f27feca5680
Tulio Tome <tuliotdct@gmail.com>
2026-04-06 17:32:23 +0200

    fix: move count calculation to avoid redundant database read in create_bronze_for_table function

--- src/medallion_architecture/bronze.py
+++ src/medallion_architecture/bronze.py
@@ -14,10 +14,10 @@ def create_bronze_for_table(table_name, execution_date=None):
 
     read_db_tables = spark.read.jdbc(url=jdbc_url, table=f'{table_name}', properties=props)
 
+    count = read_db_tables.count()
+
     read_db_tables.write.mode("overwrite").parquet(f"s3a://{bucket}/bronze/{table_name}/{execution_date}")
 
-    count = read_db_tables.count()
-    
     spark.stop()
     
     return {"table": table_name, "records": count, "status": "success"}
